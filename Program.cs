792abf01545f7ec5a65e5a615dc7e2f8f2b94b04
sonyayuninda <61997419+sonyayuninda@users.noreply.github.com>
2020-07-27 15:36:12 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Angkot.AngkotClass;
+
+namespace Angkot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kendaran kendaraan;
+
+            Console.WriteLine();
+            kendaraan = new Angkot1();
+            kendaraan.KodeAngkot();
+            kendaraan.Jurusan();
+            kendaraan.Operasi();
+
+            Console.WriteLine();
+            kendaraan = new Angkot2();
+            kendaraan.KodeAngkot();
+            kendaraan.Jurusan();
+            kendaraan.Operasi();
+
+            Console.WriteLine();
+            kendaraan = new Angkot3();
+            kendaraan.KodeAngkot();
+            kendaraan.Jurusan();
+            kendaraan.Operasi();
+
+            Console.WriteLine();
+            kendaraan = new Angkot4();
+            kendaraan.KodeAngkot();
+            kendaraan.Jurusan();
+            kendaraan.Operasi();
+
+            Console.WriteLine();
+            kendaraan = new Angkot5();
+            kendaraan.KodeAngkot();
+            kendaraan.Jurusan();
+            kendaraan.Operasi();
+
+            Console.ReadKey();
+        }
+    }
+}
